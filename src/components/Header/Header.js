8bf2e471b02b22653473de93d6a06bd546384920
Swagata2003 <swagatasinha.pri@gmail.com>
2023-05-26 17:28:14 +0530

    commit1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = () => {
       <Navbar />
       <div className='header-content flex align-center justify-center flex-column text-center'>
         <SearchForm />
-        <h1 className='text-white header-title ls-2'>What are your favorite cuisines?</h1>
+        <h1 className='text-white header-title ls-2'>Chase the flavors</h1>
         <p className='text-uppercase text-white my-3 ls-1'>personalize your experience</p>
       </div>
     </header>
